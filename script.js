9d42846ba1df7fbf5621e26f5ef3a2c2e1fbe05c
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2024-09-08 01:43:41 +0530

    updated contact section

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+document.querySelectorAll('.menu-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  });
+});
+
 
 
 document.addEventListener('scroll', () => {
